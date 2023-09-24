b475d8fc4b0e95d5914d2b8d0416e0a3b4ab4892
Kartik Tripathi <kartiktripathi.1000@gmail.com>
2023-09-24 23:42:32 +0530

    added git_data modified code of read.py and extract.py

--- extract.py
+++ extract.py
@@ -1,23 +1,21 @@
-'''import os
+import os
 import shutil
 def getListOfFiles(dirName):
     listOfFile = os.listdir(dirName)
-    allFiles = list()
+    allFiles = []
     for entry in listOfFile:
         fullPath = os.path.join(dirName, entry)
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            if(fullPath.endswith('.webp')):
-                allFiles.append(fullPath)
-                
+        elif (fullPath.endswith('.webp')):
+            allFiles.append(fullPath)
+
     return allFiles
 
-dirname = '/home/aniket/Downloads/giphy.com' #Your Full Path of Projects Folder
-dest="/home/aniket/Desktop/Projects/gif_extract/gif_data/"
+dirname = 'C:/Users/PREDATOR/Desktop/dona/dona_project_day' #Your Full Path of Projects Folder
+dest="C:/Users/PREDATOR/Desktop/dona/dona_project_day/gif_data/"
 
 data=getListOfFiles(dirname)
 for i in range(len(data)):
     fname=dest+str(i)+".webp"
     shutil.copyfile(data[i], fname)
-'''
--- read.py
+++ read.py
@@ -5,13 +5,13 @@ from textblob import TextBlob
 import webp
 import os
 import shutil
-dest="/home/aniket/Desktop/Projects/gif_extract/gif_data/"
-op_dest="/home/aniket/Desktop/Projects/gif_extract/filtered_data/"
+dest="C:/Users/PREDATOR/Desktop/dona/dona_project_day/gif_data/"
+op_dest="C:/Users/PREDATOR/Desktop/dona/dona_project_day/filtered_data/"
 for i in range(107):
     ip=dest+str(i)+".webp"
-    op=dest+"tmp.gif"
+    op = f"{dest}tmp.gif"
     anim = webp.load_images(ip)
-    anim[0].save(op, save_all=True, append_images=anim[0:1], duration=1, loop=0)
+    anim[0].save(op, save_all=True, append_images=anim[:1], duration=1, loop=0)
     im = Image.open(op)
     im.seek(0)
     im.save("tmp.png")
@@ -19,7 +19,7 @@ for i in range(107):
     try:
         crop_img = img[0:170,200:470]
         txt = pytesseract.image_to_string(crop_img)
-    except:
+    except Exception:
         txt=""
     word=txt.replace("\n", "")
     word=word.replace(" ", "")
@@ -27,10 +27,6 @@ for i in range(107):
     word=word.lower()
     b = TextBlob(word)
     ans=str(b.correct())
-    print("corrected text: "+ans)
-    if(len(ans)>0):
-        fname=op_dest+ans+".webp"
-        shutil.copyfile(ip, fname)
-    else:
-        fname=op_dest+"unknown"+str(i)+".webp"
-        shutil.copyfile(ip, fname)
+    print(f"corrected text: {ans}")
+    fname = op_dest+ans+".webp" if ans else f"{op_dest}unknown{str(i)}.webp"
+    shutil.copyfile(ip, fname)
